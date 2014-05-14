146e6fbf37c07762e4b7d22cbc5b490570150847
linux_china <linux_china@hotmail.com>
2014-05-14 19:58:04 +0800

    implement resource load

--- src/main/java/org/mvnsearch/mail/catcher/web/servlet/WebjarsServlet.java
+++ src/main/java/org/mvnsearch/mail/catcher/web/servlet/WebjarsServlet.java
@@ -1,10 +1,14 @@
 package org.mvnsearch.mail.catcher.web.servlet;
 
+import org.apache.commons.io.IOUtils;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * webjars servlet
@@ -14,6 +18,11 @@ import java.io.IOException;
 public class WebjarsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doGet(request, response);
+        String requestUri = request.getRequestURI();
+        InputStream resource = this.getClass().getResourceAsStream("/META-INF/resources" + requestUri);
+        response.setContentType("application/octet-stream");
+        ServletOutputStream output = response.getOutputStream();
+        IOUtils.copy(resource, output);
+        output.flush();
     }
 }

5d60907e2d26a2541fcc95535ab1cb833318dc41
linux_china <linux_china@hotmail.com>
2014-05-14 19:45:33 +0800

    mail catcher

---
+++ src/main/java/org/mvnsearch/MailCatcher.java
@@ -0,0 +1,45 @@
+package org.mvnsearch;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.mvnsearch.mail.catcher.MessageHandlerImpl;
+import org.mvnsearch.mail.catcher.web.servlet.WebjarsServlet;
+import org.subethamail.smtp.MessageContext;
+import org.subethamail.smtp.MessageHandler;
+import org.subethamail.smtp.MessageHandlerFactory;
+import org.subethamail.smtp.server.SMTPServer;
+
+/**
+ * mail catcher
+ *
+ * @author linux_china
+ */
+public class MailCatcher {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting mail server");
+        startSmtpServer();
+        System.out.println("Starting web server");
+        startWebServer();
+        System.out.println("Done");
+    }
+
+    public static void startSmtpServer() {
+        SMTPServer smtpServer = new SMTPServer(new MessageHandlerFactory() {
+            public MessageHandler create(MessageContext messageContext) {
+                return new MessageHandlerImpl(messageContext);
+            }
+        });
+        smtpServer.setHostName("Mail Catcher");
+        smtpServer.setPort(1025);
+        smtpServer.start();
+    }
+
+    public static void startWebServer() throws Exception {
+        Server server = new Server(8080);
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        handler.addServletWithMapping(WebjarsServlet.class, "/webjars");
+        server.start();
+        server.join();
+    }
+}

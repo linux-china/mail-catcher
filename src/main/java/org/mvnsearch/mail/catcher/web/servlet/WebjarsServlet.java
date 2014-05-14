f92e2e9a905e6aa60552208c9188ab487d5c1b22
linux_china <linux_china@hotmail.com>
2014-05-14 19:46:09 +0800

    webjars servlet

---
+++ src/main/java/org/mvnsearch/mail/catcher/web/servlet/WebjarsServlet.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.mail.catcher.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * webjars servlet
+ *
+ * @author linux_china
+ */
+public class WebjarsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+}

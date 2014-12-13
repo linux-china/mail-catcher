de4991f5c4249b431eae807cff8d5a8e2d57d608
linux_china <linux_china@hotmail.com>
2014-12-13 17:05:06 +0800

    add encoding

--- src/main/java/org/mvnsearch/mail/catcher/Application.java
+++ src/main/java/org/mvnsearch/mail/catcher/Application.java
@@ -2,7 +2,11 @@ package org.mvnsearch.mail.catcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -10,5 +14,14 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
     }
 }

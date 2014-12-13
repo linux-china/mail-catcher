6da993f1c30bda32332e6f1d454af0d9c9cd0ce9
linux_china <linux_china@hotmail.com>
2014-12-13 17:06:18 +0800

    minor Update

--- src/main/java/org/mvnsearch/mail/catcher/web/PortalController.java
+++ src/main/java/org/mvnsearch/mail/catcher/web/PortalController.java
@@ -3,6 +3,7 @@ package org.mvnsearch.mail.catcher.web;
 
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.SimpleEmail;
+import org.mvnsearch.mail.catcher.infrastructure.EncodingUtils;
 import org.mvnsearch.mail.catcher.infrastructure.WiserServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.subethamail.wiser.WiserMessage;
-
-import java.util.List;
 
 /**
  * portal controller
@@ -21,12 +20,14 @@ import java.util.List;
 @RestController
 public class PortalController {
     @Autowired
-    WiserServer wiserServer;
+    private WiserServer wiserServer;
+    private EncodingUtils encodingUtils = new EncodingUtils();
 
     @RequestMapping("/")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("allEmails", wiserServer.getMessages());
+        modelAndView.addObject("encodingUtils", encodingUtils);
         return modelAndView;
     }
 
@@ -38,24 +39,32 @@ public class PortalController {
 
     @RequestMapping("/show")
     public ModelAndView show(@RequestParam("id") String msgId) {
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("email", wiserServer.findMsgById(msgId));
+        ModelAndView modelAndView = new ModelAndView("email");
+        WiserMessage email = wiserServer.findMsgById(msgId);
+        modelAndView.addObject("email", email);
         return modelAndView;
     }
 
     @RequestMapping("/tools")
     public ModelAndView tools() {
-        return new ModelAndView("edit");
+        return new ModelAndView("send");
+    }
+
+    @RequestMapping("/clear")
+    public ModelAndView clear() {
+        wiserServer.clear();
+        return new ModelAndView("redirect:/");
     }
 
     @RequestMapping("/send")
-    public ModelAndView send(@RequestParam("from") String from, @RequestParam("to") String to,
+    public ModelAndView send(@RequestParam("sender") String sender, @RequestParam("receiver") String receiver,
                              @RequestParam("subject") String subject, @RequestParam("body") String body) {
         try {
             Email email = constructEmail();
-            email.setFrom(from);
-            email.addTo(to);
+            email.setFrom(sender);
+            email.addTo(receiver);
             email.setSubject(subject);
+            email.setCharset("UTF-8");
             email.setMsg(body);
             email.send();
         } catch (Exception ignore) {

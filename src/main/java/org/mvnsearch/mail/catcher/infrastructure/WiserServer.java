f3f557c4852a46a84ecb50de640b1ef7322a1716
linux_china <linux_china@hotmail.com>
2014-12-13 14:16:02 +0800

    wiser server

---
+++ src/main/java/org/mvnsearch/mail/catcher/infrastructure/WiserServer.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.mail.catcher.infrastructure;
+
+import org.subethamail.wiser.WiserMessage;
+
+import java.util.List;
+
+/**
+ * wiser server
+ *
+ * @author linux_china
+ */
+public interface WiserServer {
+    /**
+     * start subethamail wiser server
+     */
+    public void start();
+
+    /**
+     * stop subethamail wiser server
+     */
+    public void stop();
+
+    /**
+     * get messages in box
+     *
+     * @return message list
+     */
+    public List<WiserMessage> getMessages();
+}

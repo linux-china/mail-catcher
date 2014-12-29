8bc6462f96d86d91c7588612c1cacb52eb5ad434
linux_china <linux_china@hotmail.com>
2014-12-29 19:55:48 +0800

    add sitemesh filter

--- src/main/java/org/mvnsearch/mail/catcher/Application.java
+++ src/main/java/org/mvnsearch/mail/catcher/Application.java
@@ -1,12 +1,18 @@
 package org.mvnsearch.mail.catcher;
 
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -24,4 +30,22 @@ public class Application {
         filter.setForceEncoding(true);
         return filter;
     }
+
+    /**
+     * sitemesh filter
+     *
+     * @return sitemesh filter
+     */
+    @Bean
+    public FilterRegistrationBean siteMeshFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new ConfigurableSiteMeshFilter() {
+            protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
+                builder.addDecoratorPath("/*", "/layout.html");
+            }
+        });
+        filterRegistrationBean.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+        return filterRegistrationBean;
+    }
+
 }

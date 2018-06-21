dbafb255a8020a252c012dbd51b56a08f16ffaf0
linux_china <linux_china@hotmail.com>
2018-06-21 16:40:33 -0700

    remove sitemesh & encoding

--- src/main/java/org/mvnsearch/mail/catcher/Application.java
+++ src/main/java/org/mvnsearch/mail/catcher/Application.java
@@ -1,49 +1,13 @@
 package org.mvnsearch.mail.catcher;
 
-import org.sitemesh.builder.SiteMeshFilterBuilder;
-import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        return filter;
-    }
-
-    /**
-     * sitemesh filter
-     *
-     * @return sitemesh filter
-     */
-    @Bean
-    public FilterRegistrationBean siteMeshFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new ConfigurableSiteMeshFilter() {
-            protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
-                builder.addDecoratorPath("/*", "/layout.html");
-            }
-        });
-        filterRegistrationBean.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
-        return filterRegistrationBean;
     }
 
 }
